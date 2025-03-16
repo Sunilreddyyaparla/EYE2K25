28b730042b9f53cdcdab315fd35f033cf6b5d8a0
Yaparla Sunil Kumar Reddy <161870912+Sunilreddyyaparla@users.noreply.github.com>
2025-03-16 18:47:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -37,7 +37,8 @@ FEST_EVENTS = {
     # Technical Events
     "Project Expo": {"fee": 300, "category": "technical"},
     "Hackathon": {"fee": 500, "category": "technical"},
-    "Poster or Paper Presentation": {"fee": 300, "category": "technical"},
+    "Paper Presentation": {"fee": 300, "category": "technical"},
+    "Poster Presentation": {"fee": 300, "category": "technical"},
     "Technical Quiz": {"fee": 700, "category": "technical"},
     "Circuit Hunt": {"fee": 300, "category": "technical"},
     "Workshop": {"fee": 400, "category": "technical"},
